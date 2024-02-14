d6b83b44f553f42e527129ea9efe06b775a2012b
MarkChen2000 <ches40128@gmail.com>
2024-02-14 23:06:53 +0900

    Added background parallel moving feature

--- Assets/Scripts/BackGroundManager.cs
+++ Assets/Scripts/BackGroundManager.cs
@@ -1,16 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BackGroundManager : MonoBehaviour
 {
-    [SerializeField] Transform StarsBG1Trans, StarsBG2Trans;
-    [SerializeField] Transform BG1Trans, BG2Trans;
-    [SerializeField] float StarsMoveSpeed = 0.05f, BGMoveSpeed = 0.01f;
+    [SerializeField] Transform BGTrans, StarsBGTrans;
+    [SerializeField] float BGMoveSpeed = 0.01f, StarsMoveSpeed = 0.05f;
 
+    Vector2 BGStartPos, StarsBGStartPos;
+    float repeatWidth;
+
+    void Awake()
+    {
+        BGStartPos = BGTrans.position;
+        repeatWidth = 128f;
+    }
 
     void Update()
     {
-        BG1Trans.position = new Vector2(BG1Trans.position.x + BGMoveSpeed, BG1Trans.position.y);
+        MoveBackGrounds();
+
+        if (BGTrans.position.x < BGStartPos.x - repeatWidth) BGTrans.position = BGStartPos;
+        if (StarsBGTrans.position.x < StarsBGStartPos.x - repeatWidth) StarsBGTrans.position = StarsBGStartPos;
+    }
+
+    void MoveBackGrounds()
+    {
+        BGTrans.Translate(Vector2.left * BGMoveSpeed * Time.deltaTime);
+        StarsBGTrans.Translate(Vector2.left * StarsMoveSpeed * Time.deltaTime);
     }
 }
