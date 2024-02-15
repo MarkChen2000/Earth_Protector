b664603818cf0b6752bb011787ebe689bb86d470
MarkChen2000 <ches40128@gmail.com>
2024-02-16 02:14:03 +0900

    Imported assets, added prefabs, fixed bugs about background.
    Imported asset for explosion effect.
    Added explosion effect and hit effect prefabs.
    Fixed the bugs and added another background of stars.

--- Assets/Scripts/BackGroundManager.cs
+++ Assets/Scripts/BackGroundManager.cs
@@ -5,29 +5,32 @@ using UnityEngine.UI;
 
 public class BackGroundManager : MonoBehaviour
 {
-    [SerializeField] Transform BGTrans, StarsBGTrans;
-    [SerializeField] float BGMoveSpeed = 0.01f, StarsMoveSpeed = 0.05f;
+    [SerializeField] Transform BGTrans, StarsBGTrans, StarsBG2Trans;
+    [SerializeField] float BGMoveSpeed = 3f, StarsBGSpeed = 15f, StarsBG2Speed = 10f;
 
-    Vector2 BGStartPos, StarsBGStartPos;
-    float repeatWidth;
+    Vector2 BGStartPos, StarsBGStartPos, StarsBG2StarsPos;
+    [SerializeField] float BGRepeatWidth = 128f, StarsBGRepeatWidth = 128f, StarsBG2RepeatWidth = 136f; // the width of background in moving direction and devided by 2.
 
     void Awake()
     {
         BGStartPos = BGTrans.position;
-        repeatWidth = 128f; // the width of background in moving direction and devided by 2.
+        StarsBGStartPos = StarsBGTrans.position;
+        StarsBG2StarsPos = StarsBG2Trans.position;
     }
 
     void Update()
     {
         MoveBackGrounds();
 
-        if (BGTrans.position.x < BGStartPos.x - repeatWidth) BGTrans.position = BGStartPos;
-        if (StarsBGTrans.position.x < StarsBGStartPos.x - repeatWidth) StarsBGTrans.position = StarsBGStartPos;
+        if (BGTrans.position.x < BGStartPos.x - BGRepeatWidth) BGTrans.position = BGStartPos;
+        if (StarsBGTrans.position.x < StarsBGStartPos.x - StarsBGRepeatWidth) StarsBGTrans.position = StarsBGStartPos;
+        if (StarsBG2Trans.position.x < StarsBG2StarsPos.x - StarsBG2RepeatWidth) StarsBG2Trans.position = StarsBG2StarsPos;
     }
 
     void MoveBackGrounds()
     {
         BGTrans.Translate(Vector2.left * BGMoveSpeed * Time.deltaTime);
-        StarsBGTrans.Translate(Vector2.left * StarsMoveSpeed * Time.deltaTime);
+        StarsBGTrans.Translate(Vector2.left * StarsBGSpeed * Time.deltaTime);
+        StarsBG2Trans.Translate(Vector2.left * StarsBG2Speed * Time.deltaTime);
     }
 }
